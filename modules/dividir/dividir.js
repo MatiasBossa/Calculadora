174719b96f656d113e06903a392c952a1e950800
Matias Bossa <matubossa@gmail.com>
2020-09-08 19:13:44 -0300

    nuevos cambios en dividir

--- modules/dividir/dividir.js
+++ modules/dividir/dividir.js
@@ -2,4 +2,4 @@
 * Creamos una Arrow Function retornando la division de los 2 parametros
 * y la expotamos 
 */
-module.exports = (num1, num2) => num1 / num2;
+module.exports = (num1, num2) => (num2 != 0) ? num1 / num2 : "Error, no se puede dividir por 0";
